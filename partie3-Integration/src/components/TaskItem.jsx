80fb9c325e982518273073648fec5399c94941f9
farahBALI <far11114566trghc@gmail.com>
2023-10-15 00:41:02 +0100

    update: TaskItem.jsx

--- partie3-Integration/src/components/TaskItem.jsx
+++ partie3-Integration/src/components/TaskItem.jsx
@@ -18,33 +18,116 @@ const TaskItem = ({ task, onToggleTask, onDeleteTask, onEditTask }) => {
     setEditedTitle(e.target.value);
   };
 
+  // Styles for the component
+  const styles = {
+    task: {
+      backgroundColor: '#fff',
+      border: '1px solid #ddd',
+      padding: '10px 20px',
+      marginBottom: '10px',
+      borderRadius: '4px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      position: 'relative',
+      fontSize: '18px',
+      color: '#333',
+    },
+    checkbox: {
+      appearance: 'none',
+      backgroundColor: '#f2f2f2',
+      border: '1px solid #cacece',
+      borderRadius: '3px',
+      position: 'absolute',
+      left: '10px',
+      top: '54%',
+      transform: 'translateY(-50%)',
+      width: '20px',
+      height: '20px',
+    },
+    checkboxChecked: {
+      backgroundColor: '#9f87c3',
+      border: '1px solid #9f87c3',
+    },
+    label: {
+      display: 'flex',
+      alignItems: 'center',
+      paddingLeft: '40px',
+    },
+    taskLabel: {
+  resize: 'none',
+  fontSize: '18px',
+  padding: '10px',
+  width:'250px' ,
+  overflowY: 'auto',
+  whiteSpace: 'wrap', 
+  wordWrap: 'break-word',
+    },
+    text: {
+      resize: 'vertical',
+      border: '1px solid #cacece',
+      borderRadius: '3px',
+      fontSize: '18px',
+      padding: '10px',
+    },
+    deleteButton: {
+      backgroundColor: 'transparent',
+      border: 'none',
+      color: '#f54242',
+      cursor: 'pointer',
+      position: 'absolute',
+      right: '10px',
+      top: '50%',
+      transform: 'translateY(-50%)',
+    },
+    editButton: {
+      backgroundColor: 'transparent',
+      border: 'none',
+      color: '#774D9E',
+      cursor: 'pointer',
+      position: 'absolute',
+      right: '40px',
+      top: '50%',
+      transform: 'translateY(-50%)',
+    },
+  };
+  const checkmarkStyles = {
+    fontSize: '14px',
+    position: 'absolute',
+    top: '55%',
+    left: '6%',
+    transform: 'translate(-50%, -50%)',
+    color: '#fff',
+    display: task.done ? 'block' : 'none'
+  };
   return (
-    <div className="task">
-      <label>
+    <div style={styles.task}>
+      <label style={styles.label}>
         <input 
           type="checkbox"
           checked={task.done}
-          className="checkbox"
+          style={task.done ? { ...styles.checkbox, ...styles.checkboxChecked } : styles.checkbox}
           onChange={() => onToggleTask(task._id)}
         />
+        <span style={checkmarkStyles}>&#10004;</span>
         {
           isEditing 
-          ? <textarea className="text" value={editedTitle} onChange={handleChange} />
-          : <span className={`taskLabel ${task.done}` ? 'done' : ''}>
+          ? <textarea style={styles.text} value={editedTitle} onChange={handleChange} />
+          : <span style={styles.taskLabel} className={task.done ? 'done' : ''}>
               {task.title}
             </span>
         }
       </label>
       { 
         isEditing 
-        ? <button className="editButton" onClick={handleSave}>
+        ? <button style={styles.editButton} onClick={handleSave}>
             <FiSave />
           </button>
-        : <button className="editButton" onClick={handleEdit}>
+        : <button style={styles.editButton} onClick={handleEdit}>
             <FiEdit />
           </button>
       }
-      <button className="deleteButton" onClick={() => onDeleteTask(task._id)}>
+      <button style={styles.deleteButton} onClick={() => onDeleteTask(task._id)}>
         <FiTrash2 />
       </button>
     </div>
@@ -52,96 +135,3 @@ const TaskItem = ({ task, onToggleTask, onDeleteTask, onEditTask }) => {
 };
 
 export default TaskItem;
-
-const styles = `
-
-.task {
-  background-color: #fff;
-  border: 1px solid #ddd;
-  padding: 10px 20px;
-  margin-bottom: 10px;
-  border-radius: 4px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  position: relative;
-  font-size: 18px;
-  color: #333;
-}
-
-.checkbox {
-  appearance: none;
-  background-color: #f2f2f2;
-  border: 1px solid #cacece;
-  padding: 10px;
-  border-radius: 3px;
-  position: absolute; 
-  left: 10px; 
-  top: 50%; 
-  transform: translateY(-50%); 
-}
-
-.checkbox:checked {
-  background-color: #9f87c3;
-  border: 1px solid #9f87c3;
-  color: #99a1a7;
-}
-
-.checkbox:checked:after {
-  content: '\\2714';
-  font-size: 14px;
-  position: absolute;
-  top: 0px;
-  left: 3px;
-  color: #fff;
-}
-
-label {
-  display: flex;
-  align-items: center;
-  flex: 1;
-  padding-left: 400px;
-}
-
-.taskLabel, text {
-  flex: 1;
-  margin: 10px;
-  margin-right: 40px;
-  overflow: hidden;
-  white-space: wrap;
-  text-overflow: ellipsis;
-  max-width: 80%; 
-}
-
-.text{
-  resize: vertical;
-  border: 1px solid #cacece;
-  border-radius: 3px;
-  font-size: 18px;
-  padding: 10px;
-}
-.deleteButton {
-  background-color: transparent;
-  border: none;
-  color: #f54242;
-  cursor: pointer;
-  position: absolute;
-  right: 10px; 
-  top: 50%; 
-  transform: translateY(-50%); 
-}
-
-.editButton {
-  background-color: transparent;
-  border: none;
-  color: #774D9E;
-  cursor: pointer;
-  position: absolute; 
-  right: 40px; 
-  top: 50%; 
-  transform: translateY(-50%); 
-}
-
-`;
-
-// document.head.insertAdjacentHTML('beforeend', <style>${styles}</style>);
